a6a34cb43e19146a8f8053155405c38faaeec9ff
bgerold <bgerold@LABSEC11-2YVN>
2018-11-08 11:04:26 -0600

    Added an asynchronous decorator class so uploading to the database does not hang.

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CurrencyTrader.Contracts;
+
+namespace CurrencyTrader.AdoNet
+{
+    public class AsynchTradeStorage : ITradeStorage
+    {
+        AdoNetTradeStorage synchTradeStorage;
+        public AsynchTradeStorage(ILogger logger)
+        {
+            synchTradeStorage = new AdoNetTradeStorage(logger);
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            Task.Run(() => synchTradeStorage.Persist(trades));
+        }
+    }
+}
